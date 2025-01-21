b33194723fe851cfae74bc7abde709c8ce13204e
Gabriela Kotseva <gkotseva98@gmail.com>
2025-01-21 16:52:45 +0200

    Component added to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import {Route, Routes} from 'react-router-dom'
 import { Home } from './pages/Customer/Home/Home'
 import { paths } from './paths/customer'
+import { Footer } from './components/Footer'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path={paths.home} element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path={paths.home} element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
