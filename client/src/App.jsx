371bf845abcd9662b2b4ba231b07e34024ce364b
Gabriela Kotseva <gkotseva98@gmail.com>
2025-01-21 16:46:41 +0200

    Added paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+import {Route, Routes} from 'react-router-dom'
+import { Home } from './pages/Customer/Home/Home'
+import { paths } from './paths/customer'
+
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path={paths.home} element={<Home />} />
+    </Routes>
   )
 }
 
