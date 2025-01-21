f6732c488be6034d569181d3fff43eda8a88b510
Gabriela Kotseva <gkotseva98@gmail.com>
2025-01-21 16:52:38 +0200

    New component created

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.modules.css'
+
+export function Footer () {
+    return (
+        <div className="footer-container">
+            <p>	&#169; <span>ПетВиа</span> Всички права са запазени!</p>
+        </div>
+    )
+}
