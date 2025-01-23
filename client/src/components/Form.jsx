f71c3c76a495811f4897e09a6f02d954fb30b956
Gabriela Kotseva <gkotseva98@gmail.com>
2025-01-23 17:53:14 +0200

    New component created

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,33 @@
+import './Form.modules.css'
+
+export function Form() {
+    const formName = 'register'
+
+    const forms = {
+        login: [
+            { type: "email", label: "Имейл" },
+            { type: "password", label: "Парола" }
+        ],
+        register: [
+            { type: "text", label: "Име" },
+            { type: "text", label: "Фамилия" },
+            { type: "email", label: "Имейл" },
+            { type: "password", label: "Парола" },
+            { type: "password", label: "Повтори парола" }
+        ]
+    };
+
+    return (
+        <div className="form-container">
+            <form>
+                {forms[formName].map(el => (
+                    <>
+                        <label>{el.label}</label>
+                        <input type={el.type}></input>
+                    </>
+                ))}
+                <button>{formName === "login" ? "Влез" : "Регистрация"}</button>
+            </form>
+        </div>
+    );
+}
