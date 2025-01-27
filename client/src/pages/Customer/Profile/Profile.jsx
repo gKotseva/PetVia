e6bb07dfa8b2ad5a909ccf5910601b76804b1d2a
Gabriela Kotseva <gkotseva98@gmail.com>
2025-01-27 17:11:23 +0200

    New component + styling created

--- client/src/pages/Customer/Profile/Profile.jsx
+++ client/src/pages/Customer/Profile/Profile.jsx
@@ -1,9 +1,24 @@
 import { useState } from 'react'
 import './Profile.modules.css'
 
-export function Profile () {
-    const [outstanding, setOutstanding] = useState([])
-    const [past, setPast] = useState([])
+export function Profile() {
+    const [outstanding, setOutstanding] = useState([
+        { img: "booking.png", placeName: "Test1", date: "01/02/2025", hour: "10:00" },
+        { img: "booking.png", placeName: "Test2", date: "02/02/2025", hour: "11:30" },
+        { img: "booking.png", placeName: "Test3", date: "03/02/2025", hour: "14:45" },
+        { img: "booking.png", placeName: "Test4", date: "04/02/2025", hour: "16:00" },
+        { img: "booking.png", placeName: "Test5", date: "05/02/2025", hour: "18:15" },
+        { img: "booking.png", placeName: "Test6", date: "06/02/2025", hour: "20:30" },
+
+    ])
+    const [past, setPast] = useState([
+        { img: "booking.png", placeName: "Test1", date: "01/02/2025", hour: "10:00" },
+        { img: "booking.png", placeName: "Test2", date: "02/02/2025", hour: "11:30" },
+        { img: "booking.png", placeName: "Test3", date: "03/02/2025", hour: "14:45" },
+        { img: "booking.png", placeName: "Test4", date: "04/02/2025", hour: "16:00" },
+        { img: "booking.png", placeName: "Test5", date: "05/02/2025", hour: "18:15" },
+        { img: "booking.png", placeName: "Test6", date: "06/02/2025", hour: "20:30" },
+    ])
 
     return (
         <div className="customer-profile-container">
@@ -20,18 +35,38 @@ export function Profile () {
                     <input type='password'></input>
                     <button>Запази</button>
                 </form>
-                <button>Изтриване на моя профил</button>
+                <button className='delete-button'>Изтриване на профил</button>
             </div>
             <div className="customer-outstanding-appointments">
                 {outstanding.length > 0 ? (
-                    <h2>Предстоящи часове</h2>
+                    <div className='places-container'>
+                        <h2>Предстоящи часове</h2>
+                            {outstanding.map(e => (
+                                <div className='place-details'>
+                                    <img src={e.img} alt={e.placeName} />
+                                    <h3>{e.placeName}</h3>
+                                    <h4>{e.date}</h4>
+                                    <h4>{e.hour}</h4>
+                                </div>
+                            ))}
+                    </div>
                 ) : (
                     <h2>Нямате предстоящи часове!</h2>
                 )}
             </div>
             <div className="customer-past-appointments">
                 {past.length > 0 ? (
+                    <div className='places-container'>
                     <h2>Минали часове</h2>
+                        {past.map(e => (
+                            <div className='place-details'>
+                                <img src={e.img} alt={e.placeName} />
+                                <h3>{e.placeName}</h3>
+                                <h4>{e.date}</h4>
+                                <h4>{e.hour}</h4>
+                            </div>
+                        ))}
+                    </div>
                 ) : (
                     <h2>Нямате минали часове!</h2>
                 )}
