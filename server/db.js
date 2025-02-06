272a215b46884868a31741053754fe9e55aee73e
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-06 16:16:32 +0200

    Added the database config

---
+++ server/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'petvia'
+});
+
+function executeQuery(sql) {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, function(error, results, fields) {
+            if (error) {
+                console.error('Error executing query: ' + error.message);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+module.exports = {
+    executeQuery: executeQuery
+};
