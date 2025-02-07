d12345955e4c371f7d2d97182e5f7133e2aed1ba
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-07 17:55:45 +0200

    Updated component issue
    The modal was closing no matter where you click

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -3,7 +3,7 @@ import "./Modal.modules.css";
 export function Modal({ children, onClose }) {
     return (
         <div className="modal-backdrop" onClick={onClose}>
-            <div className="modal-content">
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <p className="modal-close" onClick={onClose}>
                     ✖
                 </p>
