d9c952a45738cead0b4a19f405b6fae662cc13b1
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-11 14:09:19 +0200

    Requests created to gather information on salons for dropdown menu

--- client/src/pages/Customer/Home/Home.jsx
+++ client/src/pages/Customer/Home/Home.jsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from 'react';
 import './Home.modules.css';
+import { getAllCities, getAllStates } from '../../../handlers/salonHandler';
 
 export function Home() {
+    const [states, setStates] = useState([])
+    const [cities, setCities] = useState([])
+
+    useEffect(() => {
+        const getStates = async () => {
+            const result = await getAllStates()
+            setStates(result)
+        }
+        getStates()
+
+    }, [])
+
+    const onChange = async (e) => {
+        const result = await getAllCities(e.target.value)
+        setCities(result)
+    }
+
     return (
         <div className="home-container">
             <div className="home-section-1">
                 <div className='home-form-container'>
                     <h3>Book an appointment for a fluffy refresh</h3>
                     <form>
-                        <select name="location" id="location">
-                            <option value="" disabled selected hidden>Select a city</option>
-                            <option value="sofia">Sofia</option>
-                            <option value="varna">Varna</option>
-                            <option value="burgas">Burgas</option>
-                            <option value="plovdiv">Plovdiv</option>
+                        <select name="state" id="state" onChange={onChange}>
+                            <option value="" disabled selected>Select a state</option>
+                            {states.map(e => (
+                                <option value={e.state}>{e.state}</option>
+                            ))}
+                        </select>
+                        <select name="city" id="city">
+                            <option value="" disabled selected>Select a city</option>
+                            {cities.map(e => (
+                                <option value={e.city}>{e.city}</option>
+                            ))}
                         </select>
                         <select name="service" id="service">
                             <option value="" disabled selected hidden>Select a service</option>
