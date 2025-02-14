40e80bd7a1f7dd3f2e0da0aeb373ebfd0c1bdd1f
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-14 15:42:12 +0200

    Updated the team section

--- client/src/pages/Customer/Salon/Profile/SalonProfile.jsx
+++ client/src/pages/Customer/Salon/Profile/SalonProfile.jsx
@@ -1,39 +1,38 @@
 import './SalonProfile.modules.css'
 
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { IoStar } from "react-icons/io5";
 import { IoIosArrowForward } from "react-icons/io";
 import { DateContext } from '../../../../context/DateContext';
+import { useParams } from 'react-router-dom';
+import { getSalonDetails } from '../../../../handlers/salonHandler';
 
 export function SalonProfile() {
-    const [reviews, setReviews] = useState([
-        { stars: 5, name: 'Gabi', comment: "Good", date: '22/09/2024' },
-        { stars: 2, name: 'Mitko', comment: "Okay", date: '22/09/2024' },
-        { stars: 4, name: 'Magi', comment: "Very nice", date: '22/09/2024' },
-    ])
+    const { id } = useParams();
+    const [salonData, setSalonData] = useState([])
+    const [team, setTeam] = useState([])
 
-    const [team, setTeam] = useState([
-        { image: 'woman1.jpg', name: 'Gabriela' },
-        { image: 'woman2.jpg', name: 'Magdalena' },
-        { image: 'man1.jpg', name: 'Ivan' },
-        { image: 'man1.jpg', name: 'Ivan' },
-    ])
+    useEffect(() => {
+        const fetchSalonDetails = async () => {
+            const result = await getSalonDetails(id)
+            setSalonData(result)
+        }
+        fetchSalonDetails()
+    }, [id])
 
-    const [images, setImages] = useState(['salon-header-1.jpg', 'salon-header-2.jpg', 'salon-header-3.jpg', 'salon-header-4.jpg', 'salon-header-5.jpg'])
 
-    const [services, setServices] = useState([
-        { name: 'Bath and Drying', price: 35 },
-        { name: 'Haircut', price: 50 },
-        { name: 'Brushing', price: 25 },
-        { name: 'Nail Cutting', price: 22 },
-        { name: 'Ear Cleaning', price: 18 },
-        { name: 'Teeth Cleaning', price: 30 },
-        { name: 'Aromatic Bath', price: 40 },
-        { name: 'Massage', price: 45 },
-        { name: 'Deworming', price: 38 },
-        { name: 'Eye Cleaning', price: 20 },
-        { name: 'Full Grooming', price: 80 }
-    ])
+    useEffect(() => {
+        if (salonData.length > 0 && salonData[0]?.team) {
+            try {
+                setTeam(JSON.parse(salonData[0].team));
+            } catch (error) {
+                console.error("Error parsing team data:", error);
+            }
+        } else {
+            console.log("Waiting for salonData to be populated...");
+        }
+    }, [salonData]);
+
 
     const [showDates, setShowDates] = useState(null);
     const { datesOfTheMonth } = useContext(DateContext);
@@ -43,25 +42,29 @@ export function SalonProfile() {
         setSelectedServiceIndex(selectedServiceIndex === index ? null : index);
     }
 
+    console.log(salonData)
+    console.log(team)
+
     return (
-        <div className="salon-profile-container">
-            <div className={`salon-gallery ${images.length >= 5 ? "five-or-more" : "less-than-five"}`}>
+        salonData.length > 0 ?
+            <div className="salon-profile-container">
+            {/* <div className={`salon-gallery ${images.length >= 5 ? "five-or-more" : "less-than-five"}`}>
                 {images.map((e, index) => (
                     <img key={index} src={e} alt={`Salon ${index + 1}`} />
                 ))}
-            </div>
+            </div> */}
             <div className="salon-name">
                 <div className="salon-information">
-                    <h1>Fluffy Kingdom</h1>
-                    <h3>Vasil Levski 42, Plovdiv, 4000</h3>
+                    <h1>{salonData[0].name}</h1>
+                    <h3>{salonData[0].state} {salonData[0].city} {salonData[0].address}</h3>
                 </div>
-                <div className="salon-reviews-short">
+                {/* <div className="salon-reviews-short">
                     <h1><IoStar color="gold" /><IoStar color="gold" /><IoStar color="gold" /><IoStar color="gold" /></h1>
                     <h5>{reviews.length} reviews</h5>
-                </div>
+                </div> */}
             </div>
             <div className="salon-services">
-                <div className="services-wrapper">
+                {/* <div className="services-wrapper">
                     <div className="services-container">
                         {services.map((e, index) => (
                             <div key={index} className="service-container">
@@ -82,19 +85,19 @@ export function SalonProfile() {
                             ))}
                         </div>
                     )}
-                </div>
+                </div> */}
             </div>
             <div className="about-us-container">
                 <h1>Meet Us</h1>
                 <div className="container1">
                     <div className="salon-about-us">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nulla ipsa quasi corporis iste iusto at officia, nesciunt fugiat necessitatibus eligendi odio distinctio atque qui aliquam autem incidunt nisi rerum.</p>
+                        <p>{salonData[0].salon_description}</p>
                     </div>
                     <div className="salon-our-team">
                         {team.map((e) => (
-                            <div className="team-member" key={e.name}>
-                                <img src={e.image} alt={e.name} />
-                                <h1>{e.name}</h1>
+                            <div className="team-member" key={e.firstName}>
+                                <img src={'/woman1.jpg'} alt={e.firstName} />
+                                <h1>{e.firstName} {e.lastName}</h1>
                             </div>
                         ))}
                     </div>
@@ -102,7 +105,7 @@ export function SalonProfile() {
             </div>
             <div className="salon-reviews">
                 <h1>Our Reviews</h1>
-                <div>
+                {/* <div>
                     {reviews.map((e) => (
                         <div className="review-container" key={e.name}>
                             <h1>
@@ -117,8 +120,9 @@ export function SalonProfile() {
                             <p>{e.date}</p>
                         </div>
                     ))}
-                </div>
+                </div> */}
             </div>
         </div>
+        : <p>Loading</p>
     );
 }
