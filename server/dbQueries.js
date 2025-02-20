39b7d5aa54eeada884c2b224ba2961db92faa3e3
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-20 18:13:05 +0200

    Updated all queries due to updating the database structure

--- server/dbQueries.js
+++ server/dbQueries.js
@@ -32,85 +32,133 @@ exports.getAllStates = () => {
 exports.getAllServices = (state, city) => {
     return (
         `select service_name from salon_services
-        join salons on salon_services.salon_id = salons.id
+        join salons on salon_services.salon_id = salons.salon_id
         where salons.state = '${state}' and salons.city = '${city}'`
     )
 }
 
 exports.getSalonsPerData = (state, city, service) => {
     return (
-        `SELECT 
-    s.id AS salon_id,
-    s.name AS salon_name,
-    s.state,
-    s.city,
-    s.address,
-    ss.id AS service_id,
-    ss.service_name,
-    si.image,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-            'name', r.name,
-            'stars', r.stars,
-            'review', r.review
-        )
-    ) AS reviews
-FROM salons s
-JOIN salon_services ss ON ss.salon_id = s.id
-JOIN salon_images si ON si.salon_id = s.id
-LEFT JOIN salon_reviews r ON r.salon_id = s.id
-WHERE s.state = '${state}'
-  AND s.city = '${city}'
-  AND ss.service_name = '${service}'
-GROUP BY s.id, s.name, s.state, s.city, ss.id, ss.service_name, si.image;`
+        `
+        SELECT 
+        s.salon_id, 
+        s.name, 
+        s.state,
+        s.address,
+        s.city,
+        s.image, 
+        ss.service_name, 
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'review_id', sr.review_id,
+                'salon_id', sr.salon_id,
+                'date', sr.date,
+                'review', sr.review,
+                'stars', sr.stars,
+                'user_id', sr.user_id
+            )
+        ) AS reviews
+        FROM salons s
+        JOIN salon_services ss ON ss.salon_id = s.salon_id
+        JOIN salon_reviews sr ON ss.salon_id = sr.salon_id
+        WHERE s.state = '${state}'
+        AND s.city = '${city}'
+        AND ss.service_name = '${service}'
+        GROUP BY s.salon_id, ss.service_name;
+        `
     )
 }
 
 
 exports.getSalonDetails = (id) => {
     return (
-       `SELECT 
-    s.name,
-    s.state,
-    s.city,
-    s.address,
-    s.salon_description,
-    si.image,
+        `
+       SELECT 
+    s.salon_id, 
+    s.name, 
+    s.state, 
+    s.city, 
+    s.address, 
+    s.salon_description, 
+    s.image, 
+
+    -- Get unique team members
     (SELECT JSON_ARRAYAGG(
         JSON_OBJECT(
-            'firstName', t.firstName,
-            'lastName', t.lastName
+            'first_name', st.first_name,
+            'last_name', st.last_name
         )
-    ) 
-    FROM (
-        SELECT DISTINCT st.first_name AS firstName, st.last_name AS lastName
-        FROM salon_team st 
-        WHERE st.salon_id = s.id
-    ) t) AS team,
-    JSON_ARRAYAGG(
-        JSON_OBJECT(
-            'id', ss.id,
-            'name', ss.service_name,
-            'price', ss.price,  -- Added price
-            'duration', ss.duration  -- Added duration
-        )
-    ) AS services,
+    )
+    FROM salon_team st
+    WHERE st.salon_id = s.salon_id
+    GROUP BY st.salon_id) AS team,
+
     (SELECT JSON_ARRAYAGG(
         JSON_OBJECT(
-            'name', sr.name,
-            'stars', sr.stars,
+            'review_id', sr.review_id,
+            'date', sr.date,
             'review', sr.review,
-            'review_date', sr.review_date
+            'stars', sr.stars,
+            'user', JSON_OBJECT(
+                'id', u1.id,
+                'first_name', u1.first_name,
+                'last_name', u1.last_name,
+                'email', u1.email
+            )
         )
-    ) 
+    )
     FROM salon_reviews sr
-    WHERE sr.salon_id = s.id
-    GROUP BY sr.salon_id) AS reviews
-FROM salons s
-LEFT JOIN salon_images si ON si.salon_id = s.id
-LEFT JOIN salon_services ss ON ss.salon_id = s.id
-WHERE s.id = ${id}
-GROUP BY s.name, s.state, s.city, s.address, s.salon_description, si.image;
-`
+    LEFT JOIN users u1 ON sr.user_id = u1.id
+    WHERE sr.salon_id = s.salon_id
+    GROUP BY sr.salon_id) AS reviews,
+
+    (SELECT JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'service_id', ss.service_id,
+            'service_name', ss.service_name,
+            'duration', ss.duration,
+            'price', ss.price
+        )
+    )
+    FROM salon_services ss
+    WHERE ss.salon_id = s.salon_id
+    GROUP BY ss.salon_id) AS services,
+
+    (SELECT JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'schedule_id', ssc.schedule_id,
+            'date', ssc.date,
+            'start_time', ssc.start_time,
+            'end_time', ssc.end_time,
+            'break_time_start', ssc.break_time_start,
+            'break_time_end', ssc.break_time_end
+        )
+    )
+    FROM salon_schedule ssc
+    WHERE ssc.salon_id = s.salon_id
+    GROUP BY ssc.salon_id) AS schedule,
+
+    (SELECT JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'booking_id', sb.booking_id,
+            'service_id', sb.service_id,
+            'date', sb.date,
+            'start_time', sb.start_time,
+            'end_time', sb.end_time,
+            'user', JSON_OBJECT(
+                'id', u2.id,
+                'first_name', u2.first_name,
+                'last_name', u2.last_name,
+                'email', u2.email
+            )
+        )
+    )
+    FROM bookings sb
+    LEFT JOIN users u2 ON sb.user_id = u2.id
+    WHERE sb.salon_id = s.salon_id
+    GROUP BY sb.salon_id) AS bookings
+    FROM salons s
+    WHERE s.salon_id = ${id};
+        `
     )
 }
