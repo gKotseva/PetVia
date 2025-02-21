3dfb85a06c3e4cf7b04ecd7a75647a8cb4ec954b
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-21 18:25:43 +0200

    Updating the component structure and styling

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,8 +1,8 @@
 import "./Calendar.modules.css";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoArrowForwardCircle } from "react-icons/io5";
 import { IoArrowBackCircleSharp } from "react-icons/io5";
+import { getBookings, getSchedule } from "../handlers/salonHandler";
 
 export function Calendar() {
     const today = new Date();
@@ -10,15 +10,25 @@ export function Calendar() {
     const currentMonth = today.getMonth();
     const [visibleMonth, setVisibleMonth] = useState(currentMonth);
     const [isScheduleVisible, setIsScheduleVisible] = useState(false);
+    const [bookings, setBookings] = useState([]);
+    const [dayBookings, setDayBookings] = useState([]);
     const [selectedDate, setSelectedDate] = useState(null);
-    const [selectedHour, setSelectedHour] = useState(null);
-    const [startHour, setStartHour] = useState(9)
-    const [endHour, setEndHour] = useState(18)
+    const [schedule, setSchedule] = useState(null);
 
-    const months = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
+    useEffect(() => {
+        const fetchSchedule = async () => {
+            const bookings = await getBookings(1); // За конкретен салон
+            const schedule = await getSchedule(1); // За конкретен салон
+            setBookings(bookings);
+            setSchedule(schedule);
+        };
+        fetchSchedule();
+    }, []);
+
+    const months = {
+        1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
+        7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
+    };
 
     const getDaysInMonth = (year, month) => {
         return new Date(year, month + 1, 0).getDate();
@@ -32,10 +42,24 @@ export function Calendar() {
         setVisibleMonth((prev) => (prev === 11 ? 0 : prev + 1));
     };
 
-    const showSchedule = (day, month, year) => {
-        setSelectedDate({ day, month, year });
+    const showSchedule = (day, monthIndex, monthName, year) => {
+        setSelectedDate({ day, month: monthName, year });
+        const selectedDate = new Date(year, monthIndex, day);
+
+        const currentBookings = bookings.filter(e => {
+            const mysqlDate = new Date(e.date);
+            mysqlDate.setHours(0, 0, 0, 0);
+            return selectedDate.getTime() === mysqlDate.getTime();
+        });
+
+        const currentSchedule = schedule.filter(e => {
+            const mysqlDate = new Date(e.date);
+            mysqlDate.setHours(0, 0, 0, 0);
+            return selectedDate.getTime() === mysqlDate.getTime();
+        });
+
+        setDayBookings(currentBookings);
         setIsScheduleVisible(true);
-        setSelectedHour(null);
     };
 
     const isPastDate = (date) => {
@@ -44,69 +68,49 @@ export function Calendar() {
         return date < todayCopy;
     };
 
-    const availableHours = (day) => {
-        const hours = [];
-        for (let i = startHour; i < endHour; i++) {
-            const hour = i;
-            hours.push(hour);
-        }
-        return hours;
-    };
-
-    const handleHourSelect = (hour) => {
-        setSelectedHour(hour);
-    };
-
     return (
         <div className="calendar-container">
             <div className="calendar-days">
-            <div className="calendar-heading">
-                <button onClick={prevMonth} className="calendar-button">
-                    <IoArrowBackCircleSharp color="#f31559" />
-                </button>
-                <span className="calendar-header">{months[visibleMonth]} {currentYear}</span>
-                <button onClick={nextMonth} className="calendar-button">
-                    <IoArrowForwardCircle color="#f31559" />
-                </button>
-            </div>
-            <div className="calendar-month">
-                <h3 className="calendar-title">{months[visibleMonth]}</h3>
-                <div className="calendar-grid">
-                    {[...Array(getDaysInMonth(currentYear, visibleMonth))].map((_, day) => {
-                        const date = new Date(currentYear, visibleMonth, day + 1);
-                        const isPast = isPastDate(date);
-                        const isToday = date.toDateString() === today.toDateString();
-                        const isSelected = selectedDate && selectedDate.day === day + 1;
-
-                        return (
-                            <div
-                                key={day}
-                                className={`calendar-day ${isToday ? "today" : ""} ${isPast ? "past" : ""} ${isSelected ? "selected" : ""}`}
-                                onClick={() => showSchedule(day + 1, months[visibleMonth], currentYear)}
-                            >
-                                {day + 1}
-                            </div>
-                        );
-                    })}
+                <div className="calendar-heading">
+                    <button onClick={prevMonth} className="calendar-button">
+                        <IoArrowBackCircleSharp color="#f31559" />
+                    </button>
+                    <span className="calendar-header">{months[visibleMonth]} {currentYear}</span>
+                    <button onClick={nextMonth} className="calendar-button">
+                        <IoArrowForwardCircle color="#f31559" />
+                    </button>
                 </div>
-            </div>
-            </div>
-            <div className="calendar-hours">
-            {isScheduleVisible && selectedDate && (
-                <div className="hours-per-day">
-                    <span className="calendar-header">{selectedDate.day} {selectedDate.month}</span>
-                    <div className="schedule-hours">
-                        {availableHours(selectedDate.day).map((hour, index) => (
-                            <h4
-                                key={index}
-                                className={`hour-button ${selectedHour === hour ? "selected" : ""}`}
-                            >
-                                {hour > 12 ? `${hour - 12}:00 PM` : `${hour}:00 AM`} - {hour + 1 > 12 ? `${hour + 1 - 12}:00 PM` : `${hour + 1}:00 AM`}
-                            </h4>
-                        ))}
+                <div className="calendar-month">
+                    <div className="calendar-grid">
+                        {[...Array(getDaysInMonth(currentYear, visibleMonth))].map((_, day) => {
+                            const date = new Date(currentYear, visibleMonth, day + 1);
+                            const isPast = isPastDate(date);
+                            const isToday = date.toDateString() === today.toDateString();
+                            const isSelected = selectedDate && selectedDate.day === day + 1;
+
+                            return (
+                                <div
+                                    key={day}
+                                    className={`calendar-day ${isToday ? "today" : ""} ${isPast ? "past" : ""} ${isSelected ? "selected" : ""}`}
+                                    onClick={() => showSchedule(day + 1, visibleMonth, months[visibleMonth], currentYear)}
+                                >
+                                    {day + 1}
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
-            )}
+            </div>
+            <div className="calendar-hours">
+                {isScheduleVisible && selectedDate && (
+                    <div className="hours-per-day">
+                        <span className="calendar-header">{selectedDate.day} {selectedDate.month}</span>
+                        <div className="schedule-hours">
+                            <div className="available-hours">
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
