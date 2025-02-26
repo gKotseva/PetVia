7fa3558630ab775735ea740969e54629aeb4a939
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-26 15:28:43 +0200

    user context added to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,22 @@
+import { Footer } from "./components/Footer"
+import { Navigation } from "./components/Navigation"
+import { Home } from "./pages/client/Home"
+import { Salons } from "./pages/client/Salons"
+import { paths } from "./paths"
+import {Route, Routes} from 'react-router-dom'
+import { UserProvider } from "./context/userContext"
+
 function App() {
   return (
     <>
+    <UserProvider>
+    <Navigation />
+      <Routes>
+        <Route path={paths.home} element={<Home />} />
+        <Route path={paths.salons} element={<Salons />} />
+      </Routes>
+    <Footer />
+    </UserProvider>
     </>
   )
 }
