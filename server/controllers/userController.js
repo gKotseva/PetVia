b41f93a942985eb4456ca403d158cf681f5180a9
Gabriela Kotseva <gkotseva98@gmail.com>
2025-02-26 15:26:27 +0200

    added password hashing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,23 @@
 const router = require('express').Router()
 const db = require('../db');
 const { registerUser, getUserData } = require('../dbQueries');
+const { hashPassword, comparePassword } = require('../utils/hash');
 
 router.post('/login', async(req, res) => {
     const { email, password } = req.body
 
     const query = getUserData(email)
     const results = await db.executeQuery(query);
-    
+
     if (results.length === 0) {
       return res.status(400).send({ message: 'User does not exist!' });
-    } else if (results[0].password !== password) {
-      return res.status(400).send({ message: 'Wrong email or password!' });
+    } else {
+      const compare = await comparePassword(password, results[0].password)
+      if (!compare){
+        return res.status(400).send({ message: 'Wrong email or password!' });
+      }
+      return res.status(200).send({success: true, message: 'Login successful!', results})
     }
-
-    return res.status(200).send({success: true, message: 'Login successful!'})
-
 })
 
 router.post('/register', async(req, res) => {
@@ -25,15 +27,16 @@ router.post('/register', async(req, res) => {
         return res.status(400).send({ message: 'Passwords should match!' });
     }
 
+    const hash = await hashPassword(password)
+
     try {
-        const query = registerUser({ firstName, lastName, email, mobilePhone, password, confirmPassword })
-        const results = await db.executeQuery(query);
-        res.json(results);
+      const query = registerUser({ firstName, lastName, email, mobilePhone, password: hash });
+      const results = await db.executeQuery(query);
+      res.status(200).send({success: true, message: 'Registration successful!', results})
       } catch (error) {
         console.error('Error executing query:', error);
         res.status(500).json({ error: 'Internal server error' });
       }
 })
-
 
 module.exports = router
