9138fcae65b6350966e66ec3454b3ef2de7f4869
Gabriela Kotseva <gkotseva98@gmail.com>
2025-03-05 16:28:17 +0200

    added utility function to the for sorting out bookings and schedules

--- client/src/utils/calendar.js
+++ client/src/utils/calendar.js
@@ -15,4 +15,62 @@ export const isPastDate = (date) => {
     return date < todayCopy;
 };
 
+export const dayScheduleBookings = (scheduleDays, bookings) => {
+    const appointments = {}
+
+    scheduleDays.map(el => {
+        const currentDay = el.date
+        appointments[currentDay] = {
+            open_time: el.start_time,
+            closing_time: el.end_time,
+            break_start: el.break_time_start,
+            break_end: el.break_time_end,
+            appointments: []
+        }
+    })
+
+    Object.keys(appointments).forEach(date => {
+        const { open_time, closing_time, break_start, break_end } = appointments[date]
+
+        const slots = []
+        let currentTime = new Date(`${date}T${open_time}`)
+        const endTime = new Date(`${date}T${closing_time}`)
+        const breakStart = new Date(`${date}T${break_start}`)
+        const breakEnd = new Date(`${date}T${break_end}`)
+
+        while (currentTime < endTime) {
+            let nextTime = new Date(currentTime.getTime() + 30 * 60000)
+
+            let slot = {
+                start: currentTime.toTimeString().slice(0, 5),
+                end: nextTime.toTimeString().slice(0, 5),
+                status: "free",
+                booking: null
+            }
+
+            if (currentTime >= breakStart && currentTime < breakEnd) {
+                slot.status = "break"
+            }
+
+            bookings.forEach(booking => {
+                let bookingStart = new Date(`${date}T${booking.start_time}`)
+                let bookingEnd = new Date(bookingStart.getTime() + booking.duration * 60000)
+
+                if (currentTime >= bookingStart && currentTime < bookingEnd && booking.date === date) {
+                    slot.status = "booked"
+                    slot.booking = booking
+                }
+            })
+
+            slots.push(slot)
+            currentTime = nextTime
+        }
+        appointments[date].appointments = slots
+    })
+
+    return appointments;
+}
+
+
+
   
