3b05e202c9e53d06f814f4499f8cce446cc833a1
Gabriela Kotseva <gkotseva98@gmail.com>
2025-03-06 17:39:20 +0200

    Updated component structure and styling

--- client/src/pages/salon/SalonCalendar.jsx
+++ client/src/pages/salon/SalonCalendar.jsx
@@ -3,31 +3,28 @@ import "./SalonCalendar.modules.css";
 import { useEffect, useState } from "react";
 import { IoArrowForwardCircle } from "react-icons/io5";
 import { IoArrowBackCircleSharp } from "react-icons/io5";
-import { getBookings, getSchedule } from "../../handlers/salonHandlers";
-import { dayScheduleBookings, isPastDate, months, today } from "../../utils/calendar";
+import { getBookings, getSchedule, getSigleServiceInfo } from "../../handlers/salonHandlers";
+import { checkAvailableSlots, checkDateStatus, dayScheduleBookings, formatDate, getFullMonth, today } from "../../utils/calendar";
 
-export function SalonCalendar() {
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth();
-    const [visibleMonth, setVisibleMonth] = useState(currentMonth);
-    const [isScheduleVisible, setIsScheduleVisible] = useState(false);
+export function SalonCalendar({ serviceId, salonId }) {
+    const [visibleMonth, setVisibleMonth] = useState(today.getMonth());
     const [dayBookings, setDayBookings] = useState({});
-    const [selectedDate, setSelectedDate] = useState(null);
+    const [isScheduleVisible, setIsScheduleVisible] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(today);
+
 
     useEffect(() => {
         const fetchSchedule = async () => {
-            const fetchBookings = await getBookings(1);
-            const fetchSchedule = await getSchedule(1);
+            const fetchBookings = await getBookings(salonId);
+            const fetchSchedule = await getSchedule(salonId);
+            const fetchServiceInfo = await getSigleServiceInfo(serviceId, salonId)
 
             const bookings = dayScheduleBookings(fetchSchedule, fetchBookings)
+            // const isAvailable = checkAvailableSlots(bookings, fetchServiceInfo, selectedDate)
             setDayBookings(bookings)
         };
         fetchSchedule();
     }, []);
-
-    const getDaysInMonth = (year, month) => {
-        return new Date(year, month + 1, 0).getDate();
-    };
 
     const prevMonth = () => {
         setVisibleMonth((prev) => (prev === 0 ? 11 : prev - 1));
@@ -37,9 +34,12 @@ export function SalonCalendar() {
         setVisibleMonth((prev) => (prev === 11 ? 0 : prev + 1));
     };
 
-    const showSchedule = (day, monthIndex, monthName, year) => {
-        setSelectedDate({ day, month: monthName, year });
-        const selectedDate = new Date(year, monthIndex, day);
+    const getDaysInMonth = (year, month) => {
+        return new Date(year, month + 1, 0).getDate();
+    };
+
+    const showSchedule = (date) => {
+        setSelectedDate(date)
         setIsScheduleVisible(true);
     };
 
@@ -50,29 +50,25 @@ export function SalonCalendar() {
                     <button onClick={prevMonth} className="calendar-button">
                         <IoArrowBackCircleSharp />
                     </button>
-                    <span className="calendar-header">{months[visibleMonth]} {currentYear}</span>
+                    <span className="calendar-header">{getFullMonth(visibleMonth)} {today.getFullYear()}</span>
                     <button onClick={nextMonth} className="calendar-button">
                         <IoArrowForwardCircle />
                     </button>
                 </div>
                 <div className="calendar-month">
                     <div className="calendar-grid">
-                        {[...Array(getDaysInMonth(currentYear, visibleMonth))].map((_, day) => {
-                            const date = new Date(currentYear, visibleMonth, day + 1);
-
-                            const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
-
-                            const isPast = isPastDate(date);
-                            const isToday = date.toDateString() === today.toDateString();
-                            const isSelected = selectedDate && selectedDate.day === day + 1;
-
+                        {[...Array(getDaysInMonth(today.getFullYear(), visibleMonth))].map((_, day) => {
+                            const date = new Date(today.getFullYear(), visibleMonth, day + 1);
+                            const formattedDate = formatDate(date)
+                            const dateStatus = checkDateStatus(date)
                             const isDateInBookings = formattedDate in dayBookings;
+                            const additionalClass = dateStatus.isToday ? "today" : "" || dateStatus.isPast ? "past" : "" || dateStatus.isSelected ? "selected" : "" || !isDateInBookings ? "past" : ""
 
                             return (
                                 <div
                                     key={day}
-                                    className={`calendar-day ${isToday ? "today" : ""} ${isPast ? "past" : ""} ${isSelected ? "selected" : ""} ${!isDateInBookings ? "not-working" : ""}`}
-                                    onClick={() => showSchedule(day + 1, visibleMonth, months[visibleMonth], currentYear)}
+                                    className={`calendar-day ${additionalClass}`}
+                                    onClick={() => showSchedule(new Date(today.getFullYear(), visibleMonth, day + 1))}
                                 >
                                     {day + 1}
                                 </div>
@@ -84,7 +80,7 @@ export function SalonCalendar() {
             <div className="calendar-hours">
                 {isScheduleVisible && selectedDate && (
                     <div className="hours-per-day">
-                        <span className="calendar-header">{selectedDate.day} {selectedDate.month}</span>
+                        <span className="calendar-header">{selectedDate.getDate()} {getFullMonth(selectedDate.getMonth())}</span>
                         <div className="schedule-hours">
                             <div className="available-hours">
                             </div>
