cf7915a40621166ba9155e9ed8e1d8fda03b940c
Gabriela Kotseva <gkotseva98@gmail.com>
2025-03-18 19:45:56 +0200

    Fixed controller to work with both salons and users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,49 +1,77 @@
 const router = require('express').Router()
 const db = require('../db');
-const { registerUser, getUserData, getUserDataById, getUserBookings, updateUserData} = require('../dbQueries');
+const { getUserEmail, getSalonEmail, register } = require('../dbQueries');
 const { hashPassword, comparePassword } = require('../utils/hash');
 
 router.post('/login', async(req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body.values
+    const type = req.body.type
 
-    const query = getUserData(email)
-    const results = await db.executeQuery(query);
-
-    if (results.length === 0) {
-      return res.status(400).send({ message: 'User does not exist!' });
-    } else {
-      const compare = await comparePassword(password, results[0].password)
-      if (!compare){
-        return res.status(400).send({ message: 'Wrong email or password!' });
-      }
-      const firstName = results[0].first_name
-      const lastName = results[0].last_name
-      const id = results[0].id
-      return res.status(200).send({success: true, message: 'Login successful!', results: {firstName, lastName, id} })
-    }
-})
-
-router.post('/register', async(req, res) => {
-    const { firstName, lastName, email, mobilePhone, password, confirmPassword } = req.body;
-
-    if (password !== confirmPassword) {
-        return res.status(400).send({ message: 'Passwords should match!' });
-    }
-
-    const hash = await hashPassword(password)
-
-    try {
-      const query = registerUser({ firstName, lastName, email, mobilePhone, password: hash });
+    if(type === 'customer') {
+      const query = getUserEmail(email)
       const results = await db.executeQuery(query);
-      res.status(200).send({success: true, message: 'Registration successful!', results})
-      } catch (error) {
-        console.error('Error executing query:', error);
-        res.status(500).json({ error: 'Internal server error' });
+
+      if (results.length === 0) {
+        return res.status(400).send({ message: 'User does not exist!' });
+      } else {
+        const compare = await comparePassword(password, results[0].password)
+        if (!compare){
+          return res.status(400).send({ message: 'Wrong email or password!' });
+        }
+        const firstName = results[0].first_name
+        const lastName = results[0].last_name
+        const id = results[0].id
+        return res.status(200).send({success: true, message: 'Login successful!', results: {firstName, lastName, id} })
       }
+    } else {
+      const query = getSalonEmail(email)
+      const results = await db.executeQuery(query);
+      if (results.length === 0) {
+        return res.status(400).send({ message: 'Salon does not exist!' });
+      } else {
+        const compare = await comparePassword(password, results[0].password)
+        if (!compare){
+          return res.status(400).send({ message: 'Wrong email or password!' });
+        }
+        const salonID = results[0].salon_id
+        return res.status(200).send({success: true, message: 'Login successful!', results: {salonID} })
+      }
+    }
 })
 
-router.get('/getAllUserData', async(req, res) => {
-  const {id} = req.query
+router.post('/register', async (req, res) => {
+  const { type, values } = req.body;
+
+  const { password, confirmPassword } = values;
+  if (password !== confirmPassword) {
+    return res.status(400).send({ message: 'Passwords should match!' });
+  }
+
+  let userData = {};
+  if (type === 'customer') {
+    const { firstName, lastName, email, mobilePhone } = values;
+    userData = { firstName, lastName, email, mobilePhone, password };
+  } else {
+    const { email } = values;
+    userData = { email, password };
+  }
+
+  const hashedPassword = await hashPassword(password);
+  userData.password = hashedPassword;
+
+  try {
+    const query = register(userData, type);
+    const results = await db.executeQuery(query);
+    res.status(200).send({ success: true, message: 'Registration successful!', results });
+
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.get('/getAllUserData', async (req, res) => {
+  const { id } = req.query
 
   const query = getUserDataById(id)
   const results = await db.executeQuery(query);
@@ -51,7 +79,7 @@ router.get('/getAllUserData', async(req, res) => {
   const bookingsQuery = getUserBookings(id)
   const bookings = await db.executeQuery(bookingsQuery)
 
-  res.status(200).send({userData: results[0], bookings: bookings})
+  res.status(200).send({ userData: results[0], bookings: bookings })
 
 })
 
@@ -64,7 +92,7 @@ router.put('/updateUserData', async (req, res) => {
 
     if (fields.includes('password')) {
       const passwordIndex = fields.indexOf('password');
-      
+
       const hashedPassword = await hashPassword(values[passwordIndex]);
 
       values[passwordIndex] = hashedPassword;
@@ -82,6 +110,5 @@ router.put('/updateUserData', async (req, res) => {
     res.status(500).send({ message: 'An error occurred while updating user data' });
   }
 });
-
 
 module.exports = router
