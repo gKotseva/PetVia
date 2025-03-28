f84f30ade265eb3c4a7315c5ff5b5c2577a6b148
Gabriela Kotseva <gkotseva98@gmail.com>
2025-03-28 17:25:05 +0200

    Updated controllers with accurate data

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,72 +1,74 @@
 const router = require('express').Router()
-const db = require('../db');
-const { getUserEmail, getSalonEmail, register } = require('../dbQueries');
+const { register, checkEmail } = require('../db/authenticationQueries');
+const db = require('../db/db');
 const { hashPassword, comparePassword } = require('../utils/hash');
 
-router.post('/login', async(req, res) => {
-    const { email, password } = req.body.values
-    const type = req.body.type
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body.values
+  const accountType = req.body.accountType
 
-    if(type === 'customer') {
-      const query = getUserEmail(email)
-      const results = await db.executeQuery(query);
+  const query = checkEmail(email, accountType)
+  const checkEmailResult = await db.executeQuery(query);
 
-      if (results.length === 0) {
-        return res.status(400).send({ message: 'User does not exist!' });
-      } else {
-        const compare = await comparePassword(password, results[0].password)
-        if (!compare){
-          return res.status(400).send({ message: 'Wrong email or password!' });
-        }
-        const firstName = results[0].first_name
-        const lastName = results[0].last_name
-        const id = results[0].user_id
-        return res.status(200).send({message: 'Login successful!', results: {firstName, lastName, id} })
-      }
-    } else {
-      const query = getSalonEmail(email)
-      const results = await db.executeQuery(query);
-      if (results.length === 0) {
-        return res.status(400).send({ message: 'Salon does not exist!' });
-      } else {
-        const compare = await comparePassword(password, results[0].password)
-        if (!compare){
-          return res.status(400).send({ message: 'Wrong email or password!' });
-        }
-        const salonID = results[0].salon_id
-        return res.status(200).send({message: 'Login successful!', results: {salonID} })
-      }
+  if (checkEmailResult.length === 0) {
+    return res.status(400).send({ message: 'User does not exist!' });
+  } else {
+    const compare = await comparePassword(password, checkEmailResult[0].password)
+    if (!compare) {
+      return res.status(400).send({ message: 'Wrong email or password!' });
     }
+  }
+
+  if (accountType === 'customer') {
+    const first_name = checkEmailResult[0].first_name
+    const last_name = checkEmailResult[0].last_name
+    const id = checkEmailResult[0].user_id
+    return res.status(200).send({ message: 'Login successful!', results: { first_name, last_name, id } })
+
+  } else {
+    const salon_name = checkEmailResult[0].name
+    const salonId = checkEmailResult[0].salon_id
+    return res.status(200).send({ message: 'Login successful!', results: { salon_name, salonId } })
+
+  }
 })
 
 router.post('/register', async (req, res) => {
-  const { type, values } = req.body;
-
-  const { password, confirmPassword } = values;
-  if (password !== confirmPassword) {
-    return res.status(400).send({ message: 'Passwords should match!' });
-  }
-
+  const { accountType, values } = req.body;
+  const { password, confirm_password } = values;
   let userData = {};
-  if (type === 'customer') {
-    const { firstName, lastName, email, mobilePhone } = values;
-    userData = { firstName, lastName, email, mobilePhone, password };
+
+  if (accountType === 'customer') {
+    const { first_name, last_name, email, phone_number } = values;
+    userData = { first_name, last_name, email, phone_number };
   } else {
-    const { email } = values;
-    userData = { email, password };
+    const { salon_name, email, phone_number, state, city, address } = values;
+    userData = { salon_name, email, phone_number, state, city, address };
   }
 
   const hashedPassword = await hashPassword(password);
   userData.password = hashedPassword;
 
   try {
-    const query = register(userData, type);
-    const results = await db.executeQuery(query);
-    res.status(200).send({ success: true, message: 'Registration successful!', results });
+    const emailCheckQuery = await checkEmail(userData.email, accountType)
+    const emailCheck = await db.executeQuery(emailCheckQuery)
 
+    if (emailCheck.length > 0) {
+      return res.status(400).json({ message: 'Email is already in use' });
+    }
+
+    if (password !== confirm_password) {
+      const error = new Error('Passwords must match!');
+      error.status = 400;
+      throw error;
+    }
+
+    const query = register(userData, accountType)
+    const result = await db.executeQuery(query)
+
+    res.status(200).send({ message: 'Registration successful!', result });
   } catch (error) {
-    console.error('Error executing query:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(error).json({ error })
   }
 });
 
--- server/controllers/salonController.js
+++ server/controllers/salonController.js
@@ -1,191 +1,57 @@
+const { getDetails, editDetails, getTeam, addTeamMember, deleteTeamMember } = require('../db/salonQueries')
+const db = require('../db/db');
 const router = require('express').Router()
-const db = require('../db');
-const { getAllSalons, getTeam, getAllCities, getAllStates, deleteService, getServices, getAllServices, getSalonsPerData, getSalonDetails, getSalonBookings, getSalonSchedule, getSingleServiceInfo, insertBooking, insertTeamMember, insertService } = require('../dbQueries');
 
-router.get('/all', async(req, res) => {
-    const query = getAllSalons()
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-})
-
-router.get('/cities', async (req, res) => {
-    const {state} = req.query
-
-    const query = getAllCities(state)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-});
-
-router.get('/states', async(req, res) => {
-    const query = getAllStates()
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-})
-
-router.get('/servicesByCondition', async(req, res) => {
-    const {state, city} = req.query
-    const query = getAllServices(state, city)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-})
-
-router.get('/salonsPerData', async(req, res) => {
-    const {state, city, service} = req.query
-    const query = getSalonsPerData(state, city, service)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-})
-
-router.get('/id', async(req, res) => {
+router.get('/details', async(req, res) => {
     const {id} = req.query
-    const query = getSalonDetails(id)
-    const results = await db.executeQuery(query);
+    const query = getDetails(id)
+    const result = await db.executeQuery(query)
 
-    res.status(200).json({results});
+    res.status(200).json({data: result[0]});
 })
 
-router.get('/bookings', async(req, res) => {
-    const {id} = req.query
-    const query = getSalonBookings(id)
-    const results = await db.executeQuery(query);
+router.put('/edit-details', async(req, res) => {
+    const {id, changedFields} = req.body
 
-    res.status(200).json(results);
-})
+    const fields = Object.keys(changedFields);
+    const values = Object.values(changedFields);
 
-router.get('/schedule', async(req, res) => {
-    const {id} = req.query
-    const query = getSalonSchedule(id)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-})
-
-router.get('/singleService', async(req, res) => {
-    const {serviceId, salonId} = req.query
-    const query = getSingleServiceInfo(serviceId, salonId)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
-})
-
-router.post('/bookAppointment', async(req, res) => {
-    const {appointmentStartTime, serviceId, userId, salonId, selectedDate} = req.body
-
-    const query = insertBooking(appointmentStartTime, serviceId, userId, salonId, selectedDate)
-    const results = await db.executeQuery(query);
-
-    return res.status(200).send({success: true, message: 'Appointment Booked!', results})
-})
-
-
-// ==============================================================
-
-router.put('/updateSalonDetails', async (req, res) => {
-    try {
-        const { id, changedFields } = req.body;
-    
-        const fields = Object.keys(changedFields);
-        const values = Object.values(changedFields);
-    
-        if (fields.includes('password')) {
-          const passwordIndex = fields.indexOf('password');
-          const hashedPassword = await hashPassword(values[passwordIndex]);
-          values[passwordIndex] = hashedPassword;
-        }
-    
-        const query = `UPDATE salons SET ${fields.map(field => `${field} = ?`).join(', ')} WHERE salon_id = ?`;
-    
-        const queryParams = [...values, id];
-    
-        await db.executeQuery(query, queryParams);
-    
-        res.status(200).send({ message: 'Salon data updated successfully' });
-      } catch (err) {
-        console.error('Error executing query:', err);
-        res.status(500).send({ message: 'An error occurred while updating salon data' });
+    if (fields.includes('password')) {
+        const passwordIndex = fields.indexOf('password');
+        const hashedPassword = await hashedPassword(values[passwordIndex]);
+        values[passwordIndex] = hashedPassword;
       }
+
+      const { query, queryParams } = editDetails(id, fields, values);
+        
+      await db.executeQuery(query, queryParams);
+
+      res.status(200).send({ message: 'Salon data updated successfully' });
 })
 
 router.get('/team', async(req, res) => {
     const {id} = req.query
-
     const query = getTeam(id)
-    const results = await db.executeQuery(query);
+    const result = await db.executeQuery(query)
 
-    res.status(200).json({team: results});
+    res.status(200).json({data: result});
 })
 
-router.post('/addTeamMember', async(req, res) => {
-    const {values, id} = req.body
+router.post('/add-team-member', async(req, res) => {
+    const {id, values} = req.body
+    const query = addTeamMember(id, values)
+    const result = await db.executeQuery(query)
 
-    const query = insertTeamMember(id, values.name, values.image)
-    const results = await db.executeQuery(query);
-
-    return res.status(200).send({success: true, message: 'Team member added!', results})
+    res.status(200).json({message: `Successfully added ${values.name}!`, data: result});
 })
 
-router.get('/services', async(req, res) => {
-    const {id} = req.query
-    const query = getServices(id)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json({services: results});
-})
-
-router.post('/addService', async (req, res) => {
-    const { values, id } = req.body;
-
-    try {
-        const insertQuery = `INSERT INTO services (salon_id, name, price, duration, description) VALUES (?, ?, ?, ?, ?)`;
-        const insertResult = await db.executeQuery(insertQuery, [id, values.name, values.price, values.duration, values.description]);
-
-        const newServiceId = insertResult.insertId;
-        const fetchQuery = `SELECT * FROM services WHERE service_id = ?`;
-        const [results] = await db.executeQuery(fetchQuery, [newServiceId]);
-
-        res.status(200).json({ success: true, results, message: 'Service added!' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, error: error.message });
-    }
-});
-
-router.delete('/deleteService', async(req, res) => {
+router.delete('/delete-team-member', async(req, res) => {
     const {id} = req.body
+    const query = deleteTeamMember(id)
+    const result = await db.executeQuery(query)
 
-    const query = deleteService(id)
-    const results = await db.executeQuery(query);
-
-    res.status(200).json(results);
+    res.status(200).json({message: 'Successfully removed team member!', data: result});
 })
-
-router.put('/updateService', async (req, res) => {
-    try {
-        const { id, changedFields } = req.body;
-    
-        const fields = Object.keys(changedFields);
-        const values = Object.values(changedFields);
-    
-        const query = `UPDATE services SET ${fields.map(field => `${field} = ?`).join(', ')} WHERE service_id = ?`;
-        const queryParams = [...values, id];
-
-        await db.executeQuery(query, queryParams);
-
-        const fetchQuery = `SELECT * FROM services WHERE service_id = ?`;
-        const [result] = await db.executeQuery(fetchQuery, [id]);
-
-        res.status(200).json({ success: true, results: result });
-    } catch (err) {
-        console.error('Error executing query:', err);
-        res.status(500).json({ message: 'An error occurred while updating the service' });
-    }
-});
-
 
 
 module.exports = router
