308cbad6020f1d8c5e0930e1b49fb645e09321fa
Gabriela Kotseva <gkotseva98@gmail.com>
2025-04-01 17:57:34 +0300

    Utility functions for formatting a date and checking date status added

---
+++ server/utils/date.js
@@ -0,0 +1,28 @@
+// exports.compareDates = (first_date, second_date) => {
+//     console.log(first_date)
+//     console.log(second_date)
+// }
+
+exports.formatDate = (date) => {
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+}
+
+exports.checkDateStatus = (data) => {
+    const past = []
+    const future = []
+
+    const today = new Date()
+    const formattedToday = this.formatDate(today)
+
+    data.map(e => {
+        const formattedCurrentDay = this.formatDate(e.appointment_date)
+        e.appointment_date = formattedCurrentDay
+        if (formattedCurrentDay >= formattedToday) {
+            future.push(e)
+        } else {
+            past.push(e)
+        }
+    })
+
+    return({past: past, future: future})
+}
