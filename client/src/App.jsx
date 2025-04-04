c5e0df454a5a489b24a81708d8fc5229060795fd
Gabriela Kotseva <gkotseva98@gmail.com>
2025-04-04 16:48:02 +0300

    New component + styling created and added to app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import { NotificationProvider } from "./context/NotificationContext"
 import { SalonSettings } from "./pages/salon/SalonSettings"
 import { paths } from "./paths"
 import { ClientProfile } from "./pages/customer/ClientProfile"
+import { Home } from "./pages/shared/Home"
+import { Salons } from "./pages/shared/Salons"
+import { SalonProfile } from "./pages/shared/SalonProfile"
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
     <Routes>
         <Route path={paths.salonSettings} element={<SalonSettings />} />
         <Route path={paths.clientProfile} element={<ClientProfile />} />
+        <Route path={paths.salons} element={<Salons />} />
+        <Route path={paths.home} element={<Home />} />
+        <Route path={paths.salonProfile} element={<SalonProfile />} />
         {/* <Route path={paths.test} element={<Test />} /> */}
       </Routes>
     <Footer />
---
+++ client/src/pages/shared/SalonProfile.jsx
@@ -0,0 +1,84 @@
+import './SalonProfile.modules.css'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSalonDetails } from '../../handlers/sharedHandlers';
+import { Loading } from '../../components/Loading';
+import { displayReviewStars } from '../../components/displayReviewStars';
+import { IoIosArrowForward } from "react-icons/io";
+
+export function SalonProfile() {
+    const { id } = useParams();
+    const [salonInfo, setSalonInfo] = useState({})
+
+    useEffect(() => {
+        const fetchSalonInfo = async () => {
+            try {
+                const response = await getSalonDetails(id);
+                setSalonInfo(response.data)
+            } catch (error) {
+            }
+        };
+
+        fetchSalonInfo();
+    }, [id]);
+
+    return (
+        <div className="salon-profile-container">
+            {Object.keys(salonInfo).length > 0 ? (
+                <>
+                    <div className="salon-information">
+                        <div className="salon-contact-details">
+                            <h1>{salonInfo.salonDetails.name}</h1>
+                            <h2>{salonInfo.salonDetails.state}, {salonInfo.salonDetails.city}</h2>
+                            <h3>{salonInfo.salonDetails.address}</h3>
+                        </div>
+                        <div className="salon-review-stars">
+                            <h2>{displayReviewStars(salonInfo.averageRating)}</h2>
+                            <h3>( {salonInfo.reviews.length} reviews )</h3>
+                        </div>
+                    </div>
+                    <div className="salon-services">
+                        <div className="services-container">
+                            {salonInfo.services.map(service => (
+                                <div className="service-container" key={service.service_id}>
+                                    <h2>{service.name}</h2>
+                                    <h2>{service.duration} minutes</h2>
+                                    <h2>{service.price}$</h2>
+                                    <IoIosArrowForward color='white' />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="salon-team">
+                        <h1>Meet Our Team</h1>
+                        <div className="team-members">
+                            {salonInfo.team.map(member => (
+                                <div className="team-member" key={member.team_member_id}>
+                                    <img src='/image.png' />
+                                    <h2>{member.name}</h2>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="salon-reviews">
+                        {salonInfo?.reviews.length > 0 ? (
+                            salonInfo.reviews.map(review => (
+                                <div key={review.review_id} className="review-container">
+                                    <h1>{displayReviewStars(review.rating)}</h1>
+                                    <h2>{review.first_name} {review.last_name}</h2>
+                                    <h3>{review.created_at}</h3>
+                                    <p>{review.comment}</p>
+                                </div>
+                            ))
+                        ) : (
+                            <p>There are no reviews for this salon!</p>
+                        )}
+                    </div>
+                </>
+            ) : (
+                <Loading />
+            )}
+        </div>
+
+    )
+}
