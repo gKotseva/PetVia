1ebcae689829295bdaaa2fa8fae3866ed833b8e2
Gabriela Kotseva <gkotseva98@gmail.com>
2025-04-17 15:29:00 +0300

    Route guards added for user specific pages and 404 page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { Footer } from "./components/Footer"
 import { Navigation } from "./components/Navigation"
-import { AuthProvider } from "./context/AuthContext"
+import { useAuth } from "./context/AuthContext"
 import { NotificationProvider } from "./context/NotificationContext"
 import { SalonSettings } from "./pages/salon/SalonSettings"
 import { paths } from "./paths"
@@ -9,23 +9,39 @@ import { ClientProfile } from "./pages/customer/ClientProfile"
 import { Home } from "./pages/shared/Home"
 import { Salons } from "./pages/shared/Salons"
 import { SalonProfile } from "./pages/shared/SalonProfile"
+import { NotFound } from "./pages/shared/NotFound"
 
 function App() {
+  const { auth } = useAuth()
+
   return (
     <>
-    <NotificationProvider>
-    <AuthProvider>
-    <Navigation />
-    <Routes>
-        <Route path={paths.salonSettings} element={<SalonSettings />} />
-        <Route path={paths.clientProfile} element={<ClientProfile />} />
-        <Route path={paths.salons} element={<Salons />} />
-        <Route path={paths.home} element={<Home />} />
-        <Route path={paths.salonProfile} element={<SalonProfile />} />
-      </Routes>
-    <Footer />
-    </AuthProvider>
-    </NotificationProvider>
+      <NotificationProvider>
+        <Navigation />
+        <Routes>
+          <Route
+            path={paths.salonSettings}
+            element={
+              auth === null ? null : auth?.role === "salon"
+              ? <SalonSettings />
+              : <Navigate to="/" replace />
+            }
+          />
+          <Route
+            path={paths.clientProfile}
+            element={
+              auth === null ? null : auth?.role === "user"
+                ? <ClientProfile />
+                : <Navigate to="/" replace />
+            }
+          />
+          <Route path={paths.salons} element={<Salons />} />
+          <Route path={paths.home} element={<Home />} />
+          <Route path={paths.salonProfile} element={<SalonProfile />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </NotificationProvider>
     </>
   )
 }
