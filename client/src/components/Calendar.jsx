8657845358f086435e6effba637b253791a74fff
Gabriela Kotseva <gkotseva98@gmail.com>
2025-04-25 18:27:53 +0300

    Added logic for booking appointments from customers to the component

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,14 +1,20 @@
 import './Calendar.modules.css'
 
 import { useEffect, useState } from "react"
-import { getSchedule } from "../handlers/calendarHandlers"
+import { bookAppointment, getSchedule } from "../handlers/calendarHandlers"
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import { formatDate, getDayName, getWeekDays } from "../utils/date";
+import { Modal } from './Modal';
+import { Confirm } from './Confirm';
+import { useNotification } from '../context/NotificationContext';
 
 
-export function Calendar({ userType, salon_id, customer_id, service_duration }) {
+export function Calendar({ userType, salon_id, customer_id, service_duration, service, registeredUser }) {
     const [schedule, setSchedule] = useState([])
     const [currentWeekIndex, setCurrentWeekIndex] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedSlot, setSelectedSlot] = useState(null);
+    const { showNotification } = useNotification()
 
     const fetchSchedule = async () => {
         const response = await getSchedule(userType, salon_id, service_duration)
@@ -59,7 +65,16 @@ export function Calendar({ userType, salon_id, customer_id, service_duration }) 
                             <div className="day-card-slots">
                                 {hasSchedule &&
                                     schedule[formattedDate].slots.map((slot, index) => (
-                                        <div key={index} className={`slot ${userType} ${userType}-${slot.status}`}>
+                                        <div
+                                            key={index}
+                                            className={`slot ${userType} ${userType}-${slot.status}`}
+                                            onClick={() => {
+                                                if (slot.status === 'free') {
+                                                    setSelectedSlot({ date: formattedDate, slot });
+                                                    setShowModal(true);
+                                                }
+                                            }}
+                                        >
                                             {userType === 'salon'
                                                 ? `${slot.slot} - ${slot.status}`
                                                 : `${slot.slot}`}
@@ -70,6 +85,31 @@ export function Calendar({ userType, salon_id, customer_id, service_duration }) 
                         </div>
                     );
                 })}
+                {showModal && selectedSlot && userType === 'customer' && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <Confirm
+                            title="Confirm appointment reservation"
+                            text={
+                                <>
+                                    Are you sure you would like to book an appointment at <strong>{selectedSlot.slot.slot}</strong> on <strong>{selectedSlot.date}</strong> for "<strong>{service.name}</strong>"?
+                                </>
+                            }
+                            onConfirm={async () => {
+                                if (registeredUser === 'salon' || !registeredUser) {
+                                    showNotification('Only customers can book appointments.', 'error')
+                                    setShowModal(false)
+                                    return;
+                                }
+
+                                const response = await bookAppointment(customer_id, salon_id, service.service_id, selectedSlot.date, selectedSlot.slot.slot)
+                                showNotification(response.message, 'success')
+                                fetchSchedule()
+                                setShowModal(false);
+                            }}
+                            onDeny={() => setShowModal(false)}
+                        />
+                    </Modal>
+                )}
             </div>
         </div>
     )
