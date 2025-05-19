16bf81c0b81365ffd11740be6c6b48afa49a36b4
Gabriela Kotseva <gkotseva98@gmail.com>
2025-05-19 14:16:38 +0300

    Added cors package for handling front end requests to backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const routes = require('./router')
+const cors = require('cors');
 
 const app = express()
 const PORT = 8888
+
+app.use(cors({ origin: 'https://petvia-front.onrender.com' }));
 
 app.use(express.json())
 app.use('/', routes)
