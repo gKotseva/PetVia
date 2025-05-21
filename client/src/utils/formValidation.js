b246f0fa9fe1059cefe562f7f42d47e2e4d78016
Gabriela Kotseva <gkotseva98@gmail.com>
2025-05-21 18:44:42 +0300

    added more form validations

--- client/src/utils/formValidation.js
+++ client/src/utils/formValidation.js
@@ -1,5 +1,8 @@
-export const validate = (values) => {
+export const validate = (values, form) => {
     let error = {};
+
+    console.log(values)
+    console.log(form)
 
     for (const [name, value] of Object.entries(values)) {
         if (name === 'email') {
@@ -10,13 +13,29 @@ export const validate = (values) => {
             }
         } else if (name === 'password') {
             if (!value) {
-                error[name] = 'Email is required!'
+                error[name] = 'Password is required!'
             } else if (value.length < 6) {
                 error[name] = 'Password must be at least 6 characters.';
             }
         } else {
-            if (!value || (typeof value === 'string' && value.trim() === '')) {
-                error[name] = 'This field is required.'
+            if (
+                (!value || (typeof value === 'string' && value.trim() === '')) &&
+                !['break_start', 'break_end'].includes(name)
+            ) {
+                error[name] = 'This field is required.';
+            }
+        }
+
+        if (values.open_time && values.close_time) {
+            const [openH, openM] = values.open_time.split(':').map(Number);
+            const [closeH, closeM] = values.close_time.split(':').map(Number);
+
+            const openMinutes = openH * 60 + openM;
+            const closeMinutes = closeH * 60 + closeM;
+
+            if (openMinutes >= closeMinutes) {
+                error.open_time = 'Open time must be earlier than close time.';
+                error.close_time = 'Close time must be later than open time.';
             }
         }
     }
