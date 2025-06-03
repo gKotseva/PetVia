6d869473af8fd76c8ffa48159cf958d071807e35
Gabriela Kotseva <gkotseva98@gmail.com>
2025-06-03 17:34:30 +0300

    Updated app with new structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+
+import { paths } from "./paths"
 import { Footer } from "./components/Footer"
 import { Navigation } from "./components/Navigation"
-import { useAuth } from "./context/AuthContext"
+import { Home } from "./pages/shared/Home";
+import { Salons } from "./pages/shared/Salons";
+import { SalonProfile } from "./pages/shared/SalonProfile";
+import { NotFound } from "./pages/shared/NotFound";
+import { useAuth } from "./context/AuthContext";
+import { ClientProfile } from "./pages/customer/ClientProfile";
 import { SalonSettings } from "./pages/salon/SalonSettings"
-import { paths } from "./paths"
-import { ClientProfile } from "./pages/customer/ClientProfile"
-import { Home } from "./pages/shared/Home"
-import { Salons } from "./pages/shared/Salons"
-import { SalonProfile } from "./pages/shared/SalonProfile"
-import { NotFound } from "./pages/shared/NotFound"
 
 function App() {
   const { auth } = useAuth();
