fbd34939e06455b2ec2ddadfb3bb152941b5dc77
Gabriela Kotseva <gkotseva98@gmail.com>
2025-06-06 20:37:39 +0300

    Added test router for the testing controllers on the server

--- server/router.js
+++ server/router.js
@@ -1,15 +1,17 @@
 const router = require('express').Router()
 
-const userController = require('./controllers/userController')
-const salonController = require('./controllers/salonController')
-const authController = require('./controllers/authController')
-const sharedController = require('./controllers/sharedController')
-const calendarController = require('./controllers/calendarController')
+const userController = require('./controllers/customer')
+const salonController = require('./controllers/salon')
+const authController = require('./controllers/auth')
+const sharedController = require('./controllers/shared')
+const calendarController = require('./controllers/schedule')
+const testController = require('./controllers/testController')
 
 router.use('/api/auth', authController)
 router.use('/api/user', userController)
 router.use('/api/salon', salonController)
 router.use('/api/shared', sharedController)
-router.use('/api/calendar', calendarController)
+router.use('/api/schedule', calendarController)
+router.use('/api/test', testController)
 
 module.exports = router
